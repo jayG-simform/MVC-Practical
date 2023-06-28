a9ca9740952b946bcdf18ee77e4a0a84f6215dcf
Jay Gohel <jay.g@simformsolutions.com>
2023-06-28 10:14:25 +0530

    Added Practical-09

---
+++ Practical09/Practical09.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practical09;
+using Practical09.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Practical09.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Test3()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Test3() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Hello world",result.ViewBag.Message);
+        }
+    }
+}
---
+++ Practical09/Practical09/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical09.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Test1()
+        {
+            ViewBag.Message = "Hello world";
+            return View();
+        }
+
+        public ActionResult Test2()
+        {
+            ViewBag.Message = "Hello world";
+
+            return View();
+        }
+
+        public ActionResult Test3()
+        {
+            ViewBag.Message = "Hello world";
+
+            return View();
+        }
+    }
+}
